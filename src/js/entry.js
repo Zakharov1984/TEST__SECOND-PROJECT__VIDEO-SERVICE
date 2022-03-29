bf2e033540fd917a92ab9222d1e850cc924e5376
Zakharov <zakharov.products@gmail.com>
2022-03-29 14:29:27 +0300

    added_implementation_of_saving_the_name_on_page_reload

--- src/js/entry.js
+++ src/js/entry.js
@@ -7,7 +7,20 @@ const modalElement = document.querySelector('.modal');
 const modalClose = document.querySelector('.entry__close');
 const formLogin = document.querySelector(".entry__login");
 const formPassword = document.querySelector('.entry__password');
-const formCheckbox = document.querySelector('.entry__checkbox'); 
+const formCheckbox = document.querySelector('.entry__checkbox');
+let user1; 
+
+
+/**
+ * При событии DOMContentLoaded, делаем проверку и при условии, если пользователь вводил имя, либо редактировал имя,
+ * вставляем значение в поле "Логин", у форы.
+ * 
+ */
+document.addEventListener('DOMContentLoaded', function() {
+    if (sessionStorage.getItem('name') != undefined) {
+        formLogin.value = sessionStorage.getItem('name');
+    }
+})
 
 entryForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -24,8 +37,9 @@ modalClose.addEventListener('click', event => {
 })
 
 function authorization() {
-    let user1 = new User(formLogin.value, formPassword.value, formCheckbox.value);
+    user1 = new User(formLogin.value, formPassword.value, formCheckbox.value);
     signInLoginUser.innerText = user1.name;
+    sessionStorage.setItem('name', user1.name); 
     btnEntryOpenModal.classList.add('header__signIn-entry_disabled');
     goOutSignIn.classList.add('header__signIn-goOut_active');
 }
@@ -44,12 +58,36 @@ function User(name, password, save) {
 }
 
 
+
+/**
+ * На элемент ставим слушатель события "click", при возникновении события, вызывается обработчик,
+ * скрываем элемент с первоночальным именем пользователя, показываем элемент для ввода нового значения,
+ * в поле ввода уже дефолтно стоит старое значение, далее отслеживаем событие "change" в поля воода, как только событие срабатывает,
+ * записываем новое значение в элемент с именем пользователя, и все эелементы возвращаем на свои первоначальные места.
+ * И выполним проверку на пустое поле, если ничего не ввели, то подставим дефолтное значение.
+ */
 function changeName() {
     signInLoginUser.addEventListener('click', event => {
         signInLoginUser.classList.add('header__signIn-login_disabled');
         changeNameElement.classList.add('header__signIn-changeName_active');
         changeNameElement.value = signInLoginUser.innerText;
+        changeNameElement.addEventListener('change', event => {
+            if (!changeNameElement.value) { // если ничего не ввели, подставим дефолтное значение.
+                signInLoginUser.innerText = 'defaultName';
+            } else { // иначе записываем значение в элемент с именем и в объект, можно наоборот, и подставлять значение из поля name, объекта user1.
+                signInLoginUser.innerText = changeNameElement.value;
+                user1.name = changeNameElement.value;
+                sessionStorage.setItem('name', user1.name);
+                console.dir(user1);
+            }
+
+            signInLoginUser.classList.remove('header__signIn-login_disabled');
+            changeNameElement.classList.remove('header__signIn-changeName_active');
+
+        })
+
     })
 }
 
 changeName();
+
